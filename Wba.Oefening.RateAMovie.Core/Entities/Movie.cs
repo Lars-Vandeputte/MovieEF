6692e0d93ed4c81d213efc7d6b60f97ff851b215
Lars <113971523+Lars-Vandeputte@users.noreply.github.com>
2023-11-06 15:20:44 +0100

    Initial commit

---
+++ Wba.Oefening.RateAMovie.Core/Entities/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wba.Oefening.RateAMovie.Core.Entities
+{
+    public class Movie
+    {
+        //[Key]
+        public long Id { get; set; }
+
+        //[Required]
+        //[MaxLength(200)]
+        public string Title { get; set; }
+
+        public DateTime? ReleaseDate { get; set; }
+        //many ratings
+        public ICollection<Rating> Ratings { get; set; }
+        //one company
+        public Company Company { get; set; }
+        //unshadowed foreign key
+        public long CompanyId { get; set; }
+        //has many directors
+        public ICollection<Director> Directors { get; set; }
+        //actors
+        public ICollection<MovieActor> Actors { get; set; }
+    }
+}
---
+++ Wba.Oefening.RateAMovie.Core/Entities/Rating.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wba.Oefening.RateAMovie.Core.Entities
+{
+    public class Rating
+    {
+        public long Id { get; set; }
+
+        public byte Score { get; set; }
+
+        public string Review { get; set; }
+        //one user
+        public User User { get; set; }
+        //unshadowed foreign key
+        public long UserId { get; set; }
+        //one Movie
+        public Movie Movie { get; set; }
+        //unshadowed MovieId
+        public long MovieId { get; set; }
+    }
+}
